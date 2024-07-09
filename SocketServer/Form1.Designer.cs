2e8477b7f214d3e80a9e57a27958d23eb8a3c603
mbaibatyr <murat_b@mail.ru>
2024-07-09 19:33:52 +0500

    --

--- SocketServer/Form1.Designer.cs
+++ SocketServer/Form1.Designer.cs
@@ -30,6 +30,10 @@
         {
             listBox1 = new ListBox();
             label1 = new Label();
+            tbHost = new TextBox();
+            tbPort = new TextBox();
+            label2 = new Label();
+            button1 = new Button();
             SuspendLayout();
             // 
             // listBox1
@@ -39,23 +43,62 @@
             listBox1.ItemHeight = 15;
             listBox1.Location = new Point(0, 0);
             listBox1.Name = "listBox1";
-            listBox1.Size = new Size(320, 409);
+            listBox1.Size = new Size(328, 409);
             listBox1.TabIndex = 0;
             // 
             // label1
             // 
             label1.AutoSize = true;
-            label1.Location = new Point(68, 428);
+            label1.Location = new Point(85, 438);
             label1.Name = "label1";
-            label1.Size = new Size(38, 15);
+            label1.Size = new Size(32, 15);
             label1.TabIndex = 1;
-            label1.Text = "label1";
+            label1.Text = "Host";
+            // 
+            // tbHost
+            // 
+            tbHost.Location = new Point(119, 435);
+            tbHost.Name = "tbHost";
+            tbHost.Size = new Size(109, 23);
+            tbHost.TabIndex = 2;
+            tbHost.Text = "127.0.0.1";
+            // 
+            // tbPort
+            // 
+            tbPort.Location = new Point(119, 464);
+            tbPort.Name = "tbPort";
+            tbPort.Size = new Size(109, 23);
+            tbPort.TabIndex = 4;
+            tbPort.Text = "8080";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(85, 467);
+            label2.Name = "label2";
+            label2.Size = new Size(29, 15);
+            label2.TabIndex = 3;
+            label2.Text = "Port";
+            // 
+            // button1
+            // 
+            button1.Location = new Point(16, 506);
+            button1.Name = "button1";
+            button1.Size = new Size(98, 23);
+            button1.TabIndex = 5;
+            button1.Text = "SocketStart";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(320, 570);
+            ClientSize = new Size(328, 570);
+            Controls.Add(button1);
+            Controls.Add(tbPort);
+            Controls.Add(label2);
+            Controls.Add(tbHost);
             Controls.Add(label1);
             Controls.Add(listBox1);
             Name = "Form1";
@@ -68,5 +111,9 @@
 
         private ListBox listBox1;
         private Label label1;
+        private TextBox tbHost;
+        private TextBox tbPort;
+        private Label label2;
+        private Button button1;
     }
 }
--- SocketServer/Form1.cs
+++ SocketServer/Form1.cs
@@ -1,3 +1,8 @@
+﻿using System.Net.Sockets;
+using System.Net;
+using System.Text;
+using System.Windows.Forms;
+
 namespace SocketServer
 {
     public partial class Form1 : Form
@@ -5,6 +10,41 @@ namespace SocketServer
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            StartServer();
+        }
+
+        async Task StartServer()
+        {
+            await Task.Run(() =>
+            {
+                var ip = Dns.GetHostEntry(tbHost.Text);
+                var ad = ip.AddressList[0];
+                var ep = new IPEndPoint(ad, int.Parse(tbPort.Text));
+                Socket listener = new Socket(ad.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+                listener.Bind(ep);
+                listener.Listen(20);
+                //listBox1.Items.Add("Ñëóøàåì " + ep);
+                Socket handler;
+                while (true)
+                {
+                    byte[] bytes = new byte[1024];
+                    handler = listener.Accept();
+                    string data = null;
+                    int count = handler.Receive(bytes);
+                    var st = Encoding.UTF8.GetString(bytes, 0, count);
+                    //listBox1.Items.Add(st);
+                    data += Encoding.UTF8.GetString(bytes, 0, count);
+                    string replay = "Ñïàñèáî çà " + data;
+                    byte[] response = Encoding.UTF8.GetBytes(replay);
+                    handler.Send(response);
+                    handler.Shutdown(SocketShutdown.Both);
+                    handler.Close();
+                }
+            });
         }
     }
 }
