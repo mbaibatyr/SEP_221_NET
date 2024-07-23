322ab655329dfa932e310c1f007b8d45a9136cac
mbaibatyr <murat_b@mail.ru>
2024-07-23 20:07:57 +0500

    --

--- MyHTTPClient/Program.cs
+++ MyHTTPClient/Program.cs
@@ -4,7 +4,8 @@
     {
         static async Task Main(string[] args)
         {
-            await Method_1();
+            //await Method_1();
+            await Method_2();
         }
         static async Task<string> Method_1()
         {
@@ -12,6 +13,22 @@
             {
                 var result = await client.GetStringAsync("https://www.google.com/");
                 return result;
+            }
+        }
+
+        static async Task Method_2()
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                var result = await client.GetAsync("https://www.google.com/");
+
+                //    //var result = response.Content.ReadAsStringAsync().Result;
+                using (StreamReader sr = new StreamReader(await result.Content.ReadAsStreamAsync()))
+                {
+                    string result2 = sr.ReadToEnd();
+                    Console.WriteLine(result2);
+                }
+
             }
         }
     }
