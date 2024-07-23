054cf92add6344cbedb7f656e193c41b236b9c5a
mbaibatyr <murat_b@mail.ru>
2024-07-23 19:53:13 +0500

    --

---
+++ MyHTTPClient/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace MyHTTPClient
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+        static async Task<string> Method_1()
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                var result = await client.GetStringAsync("https://www.google.com/");
+                return result;
+            }
+        }
+    }
+}
